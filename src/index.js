c996c377468f9bf7a0a23939a36def65c6c3a8fe
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-04-16 17:35:42 +0300

    feat: add logger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
 import App from './components/app';
 import reducer from './reducer';
 
+const composeEnhenser = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  reducer,
+  composeEnhenser(applyMiddleware(logger)),
+);
 
 ReactDOM.render(
   <Provider store={store}>
